202514f870376b580789769aff38b544330e6b51
Nick Jannis Schmeißer <1848568@uni-wuppertal.de>
2019-06-21 09:54:25 +0200

    Erstellen der Klasse Draht.

---
+++ Draht.cpp
@@ -0,0 +1,18 @@
+/*
+ * Draht.cpp
+ *
+ *  Created on: Jun 21, 2019
+ *      Author: m1848568
+ */
+
+#include "Draht.h"
+
+Draht::Draht() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Draht::~Draht() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Draht.h
@@ -0,0 +1,32 @@
+/*
+ * Draht.h
+ *
+ *  Created on: Jun 21, 2019
+ *      Author: m1848568
+ */
+///Diese Klasse modelliert den Draht, der als Torsionspendel fungiert.
+/** Die Klasse speichert den Radius, die effektive Länge, das Torsionsmodul in Abhängigkeit vom Material
+ *  und das Richtmoment, was aus den übrigen Größen berechnet wird.*/
+#ifndef DRAHT_H_
+#define DRAHT_H_
+
+#include<iostream>
+#include<array>
+#include<cmath>
+using namespace std;
+
+class Draht {
+private:
+///Die Klasse speichert ihren Radius, die effektive Länge, das Torsionsmodul und das Richtmoment.
+	array<double, 2> rad;
+	array<double, 2> laen;
+	array<double, 2> torsm;
+	array<double, 2> richtm;
+public:
+	Draht();
+	Draht(const double& pRad, pLaen, pTors);
+	Draht(const double& pRad, pLaen, pTors, dRad, dLaen, dTors);
+	virtual ~Draht();
+};
+
+#endif /* DRAHT_H_ */
