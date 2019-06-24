07443f19f505da439a62b26445154f7005e851f5
Nick Jannis Schmeißer <1848568@uni-wuppertal.de>
2019-06-24 11:15:50 +0200

    Erstellen der Funktionen der Klasse Draht im Source-File

--- Draht.h
+++ Draht.h
@@ -9,6 +9,7 @@
  *  und das Richtmoment, was aus den übrigen Größen berechnet wird.*/
 #ifndef DRAHT_H_
 #define DRAHT_H_
+#define _USE_MATH_DEFINES
 
 #include<iostream>
 #include<array>
@@ -23,16 +24,28 @@ private:
 	array<double, 2> torsm;
 	array<double, 2> richtm;
 public:
+	///Objekte der Klasse können leer, mit Radius, Länge und Torsionsmodul, oder mit Radius, Länge und Torsionsmodul inklusive Fehler erstellt werden.
 	Draht();
-	Draht(const double& pRad, pLaen, pTors);
-	Draht(const double& pRad, pLaen, pTors, dRad, dLaen, dTors);
+	Draht(const double& pRad,const double& pLaen,const double& pTors);
+	Draht(const double& pRad,const double& pLaen,const double& pTors,const double& dRad,const double& dLaen,const double& dTors);
 
+	///Diese Funktion berechnet das Richtmoment des Drahtes und speichert es in der entsprechenden Variable.
+	/**Das Richtmoment wird mit der Formel D=pi*G*R^4/2L berechnet. Der Fehler wird nach Fehlerfortpflanzung berechnet. */
 	void berechneRichtm();
+	///Diese Funktion setzt den Radius des Drahtes mit Fehler.
 	void setRad(const double& pRad,const double& pFehler);
+	///Diese Funktion setzt die effektive Länge des Drahtes mit Fehler.
 	void setLaen(const double& pLaen, const double& pFehler);
+	///Diese Funktion setzt das Torsionsmodul des Drahtes mit Fehler.
+	void setTorsm(const double& pTorsm, const double& pFehler);
+	///Diese Funktion gibt den Radius des Drahtes aus.
 	array<double, 2> getRad();
+	///Diese Funktion gibt die Länge des Drahtes aus.
 	array<double, 2> getLaen();
-	array<double, 2> getTors();
+	///Diese Funktion gibt das Torsionsmodul des Drahtes aus.
+	array<double, 2> getTorsm();
+	///Diese Funktion gibt das Richtmoment des Drahtes aus.
+	/**In dieser Funktion wird zunächst die klasseneigene Funktion zur Berechnung des Richtmoments aufgerufen und gibt daraufhin die Variable richtm aus. */
 	array<double, 2> getRichtm();
 
 	virtual ~Draht();
