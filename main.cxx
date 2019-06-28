d0cfa9d73abdb919e51ab503c87d33b15ae7cec9
ronja <rovalu@gmx.de>
2019-06-28 09:50:00 +0200

    Funktion erstellePlot

--- main.cxx
+++ main.cxx
@@ -7,6 +7,7 @@
 #include"Masse.h"
 #include"Draht.h"
 #include"Pendel.h"
+#include"gnuplot-iostream.h"
 
 using namespace std;
 
@@ -14,13 +15,23 @@ Pendel pendel;
 
 int main(){}
 
-void erstellePlot(){}
+void erstellePlot(const double& pInterv, const double& pEnde){
+
+	this->erstelleDaten(pInterv, pEnde);
+	Gnuplot gp;
+	gp.setxlabel'Zeit in s';
+	gp.setylabel'Auslenkung in rad';
+	gp.settitle'Auslenkung eines Torsionspendels in Abhaengigkeit von der Zeit';
+	gp.plot"DatenPendel.txt" using 1:2:3 w xerrorbars;
+
+
+}
 
 void erstelleDaten(const double& pInterv, const double& pEnde){
 	ofstream daten;
 	array<double, 2> Auslenkung;
 	daten.open("DatenPendel.txt");
-	for(double i=0, i<pEnde, i=i+pIntervall){
+	for(double i=0, i<pEnde, i=i+pInterv){
 		Auslenkung = pendel.berechneAuslenkung(i);
 		daten<<i<<Auslenkung[0]<<Auslenkung[1]<<endl;
 	}
